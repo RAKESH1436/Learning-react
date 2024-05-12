f0340979d1492342b2de8d4f77743b19f39a5fab
rakesh1436 <sanjayrakeshgupta@gmail.com>
2024-05-13 00:09:53 +0530

    add

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,15 +1,27 @@
-import React from 'react'
-
+import React from "react";
+let name = "sanjay";
+function appss() {
+  return (
+    <div>
+      <h1>sanjay</h1>
+    </div>
+  )
+};
 function App() {
   return (
     <div>
       <h1>chai aur code......</h1>
-      <p>this is miniapps</p>
-      
-      
-    </div>
-  )
-}
+      <nav>
+        <li>home</li>
+        <li>list </li>
+        <li>number </li>
+        {appss()}
+        <li>about</li>
+      </nav>
 
+      <div className="container">this is sanjay</div>
+    </div>
+  );
+}
 
 export default App;
